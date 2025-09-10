af73a74c2f2571930f2cf69a3af91c59e9716a2f
DuragonYama <sovalye123q@gmail.com>
2025-09-10 15:47:19 +0200

    begin gemaakt aan bot & meer componenten

---
+++ BlazorApp1/GameMode.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1;
+
+/// <summary>
+/// Possible difficulties for opponent (opponent is Red)
+/// </summary>
+public enum GameMode
+{
+    TwoPlayers,
+    Easy,
+    Medium,
+    Hard
+}
+
